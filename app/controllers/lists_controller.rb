8e1d863d36545102ba162c2edb0ad1c037f35fed
316hayato <131685231+316hayato@users.noreply.github.com>
2023-05-19 15:14:18 +0000

    [Add] 復習用コメント追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,20 @@
 class ListsController < ApplicationController
-  def new
+   # 各Viewファイルに受け取った情報を送るためroutes.rbにて指定されたアクションを実行し、インスタンス変数を定義して表示させるようにする
+   # それがcontrollerの役割
+   # インスタンス変数「@*****」にはroutes.rbから渡されたデータ(URL,記録など)を格納する。
+  def new #newアクション
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @list = List.new
+    @list = List.new #newメゾット
   end
 
   def create
-    @list = List.new(list_params) # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @list = List.new(list_params) # １.&2. データを受け取り新規登録するためのインスタンス作成(定義)
     if @list.save  # 3. データをデータベースに保存するためのsaveメソッド実行
+      flash[:notice] = "投稿が成功しました" #	フラッシュメッセージを定義
       redirect_to list_path(@list.id) # 4. トップ画面へリダイレクト
     else
       render :new # render :アクション名で、同じコントローラ内の別アクションのViewを表示
+      # 仮に'new'のところが'index'だった場合、インスタンス変数@listがない(未定義)ためエラーが起きてしまう
       # エラーメッセージを扱う際にはrender、それ以外はredirect_toを使う
       # ちなみにrenderはビューファイルで書くときは[<%= render 'new' =>]とする
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
+  # ここでは自分がURLを作成するか、画面上の操作から新たにURLを作成するかでどのような動きをしていくのかを導く
+  # ここで新しく作る場合、画面でつくる場合で記載も変わる(get、postなど)
+  # URLに対しどうするか(受け取るのか追加するのか)を指定し、アクションを起こさせるようコントローラーに指示を送る
+  get '/top' => 'homes#top' #この場合URL(/top)を受け取り(get)、コントローラーにアクション(homes#top)を起こさせる
   resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
